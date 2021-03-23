b11b19ed5cd074cdc73c6214a7156a3155c25508
Stephenjoel2k <stephenjoel2k@hotmail.com>
2021-03-23 16:56:30 -0500

    Project initialization with routes

---
+++ src/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const {
+    routes: userRoutes,
+} = require('./user/routes')
+
+app.use(cors());
+app.use(express.json());
+
+app.use('/user', userRoutes);
+
+
+module.exports = app;
